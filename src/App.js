811f5f9af7548a0608b0d078e2de7e83f07d491d
Ricardo Lugo Recillas <ricardo.lugo.recillas@gmail.com>
2019-01-16 15:18:43 -0600

    comiit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 class App extends Component {
+   constructor(props){
+      super(props);
+      this.state = {
+        numero :0 
+      };
+      this.updateNumero = this.updateNumero.bind(this);
+
+   }
+
+   updateNumero(){
+    this.setState({
+      numero: this.state.numero + 1
+    });
+   }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+        <div>
+          <div>
+            <Title></Title>
+              <h2>{ this.state.numero }</h2>
+
+            <button onClick={ () => { this.updateNumero() }} > Crear cuenta Gratuita </button>
+            <div>
+              <ul>
+                <li>
+                  <h3>Calificaciones con emociones </h3>
+                  <p>Calificar tus lugares con experiencias no con numeros</p>
+                </li>
+                <li>
+                  <h3>¿Sin internet ? Sin problemas</h3>
+                  <p>Place funciona sin internet on conexion lenta </p>
+                </li>
+                <li>
+                <h3>favoritos</h3>
+                  <p> Lista de sitios favoritos  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+    render() {
+        return (
+            <div>
+            <h1>Places</h1>
+            <p>Descubre lugares simples</p>
+                
+            </div>
+        );
+    }
+}
+
+export default Title;
